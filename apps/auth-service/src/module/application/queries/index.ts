579a310937127ffdf948643669801c0ae4848ec3
MD. JeWeL <mdjewel24682468@gmail.com>
2026-06-02 00:48:40 +0600

    Add index.ts to export query handlers and queries

---
+++ apps/auth-service/src/module/application/queries/index.ts
@@ -0,0 +1,19 @@
+// Auth Queries
+export * from './auth/get-current-user.query';
+export * from './auth/get-current-user.handler';
+
+// User Queries
+export * from './user/get-user.query';
+export * from './user/get-user.handler';
+export * from './user/list-users.query';
+export * from './user/list-users.handler';
+
+// MFA Queries
+export * from './mfa/get-mfa-status.query';
+export * from './mfa/get-mfa-status.handler';
+
+// Session Queries
+export * from './session/list-sessions.query';
+export * from './session/list-sessions.handler';
+export * from './session/get-session.query';
+export * from './session/get-session.handler';
