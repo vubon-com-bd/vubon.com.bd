f6eacaf4e1a92fe625274a9f485eebe33df82902
MD. JeWeL <mdjewel24682468@gmail.com>
2026-06-03 11:55:55 +0600

    Update and rename packages/shared-constants/auth-constants/src/http-status.constants.ts to packages/shared-constants/src/http-status.constants.ts

--- packages/shared-constants/src/http-status.constants.ts
+++ packages/shared-constants/src/http-status.constants.ts
@@ -16,10 +16,7 @@
 // ============================================================
 // Type Utilities
 // ============================================================
-export type ValueOf<T> = T[keyof T];
-export type ReadonlyDeep<T> = {
-  readonly [P in keyof T]: ReadonlyDeep<T[P]>;
-};
+import type { ValueOf } from './common.types';
 
 // ============================================================
 // 1xx Informational Responses
