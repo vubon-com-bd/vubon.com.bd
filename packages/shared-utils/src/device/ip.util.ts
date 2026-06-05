b6802a56e3648c9f28a27c36e435f88201a5c4f1
MD. JeWeL <mdjewel24682468@gmail.com>
2026-06-06 00:18:06 +0600

    Remove private IP ranges and prefixes
    Removed private IP ranges and prefixes from the IP utility.

--- packages/shared-utils/src/device/ip.util.ts
+++ packages/shared-utils/src/device/ip.util.ts
@@ -21,21 +21,6 @@ const IPV4_REGEX = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]
 
 const IPV6_REGEX = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/;
 
-const PRIVATE_IPV4_RANGES = [
-  { start: '10.0.0.0', end: '10.255.255.255', description: 'Class A private' },
-  { start: '172.16.0.0', end: '172.31.255.255', description: 'Class B private' },
-  { start: '192.168.0.0', end: '192.168.255.255', description: 'Class C private' },
-  { start: '127.0.0.0', end: '127.255.255.255', description: 'Loopback' },
-  { start: '169.254.0.0', end: '169.254.255.255', description: 'Link-local' },
-];
-
-const PRIVATE_IPV6_PREFIXES = [
-  { prefix: '::1', description: 'Loopback' },
-  { prefix: 'fc00::', description: 'Unique Local (ULA)' },
-  { prefix: 'fd00::', description: 'Unique Local (ULA)' },
-  { prefix: 'fe80::', description: 'Link-local' },
-];
-
 const FORWARDED_HEADERS = [
   'x-forwarded-for',
   'x-real-ip',
