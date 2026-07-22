bd0d354de142304d0b6889a877366edd5ed94c7f
Md. JeWeL <mdjewel24682468@gmail.com>
2026-07-22 23:37:58 +0600

    fix(auth-shared-config): add eslint disable for unsafe call in env validation

--- packages/auth/shared-config/src/env/env.validation.ts
+++ packages/auth/shared-config/src/env/env.validation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable security/detect-object-injection */
 /**
  * Environment variable loader and validator
