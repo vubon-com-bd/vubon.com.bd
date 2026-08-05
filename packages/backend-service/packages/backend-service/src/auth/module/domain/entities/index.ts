af632b40c24840a9e6aebd1ef0b87765aed0396b
Md. JeWeL <mdjewel24682468@gmail.com>
2026-08-05 20:00:07 +0600

    feat(auth): add base entity and exports for domain entities

---
+++ packages/backend-service/packages/backend-service/src/auth/module/domain/entities/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Entities Exports
+ * Central export point for all entities in the auth domain
+ */
+
+// Export base entity
+export { BaseEntity } from './base.entity';
+
+// Export types
+export type { BaseEntity } from './base.entity';
+
+// Note: Import and export other entities as they are created
+// export { User } from './user.entity';
+// export { Session } from './session.entity';
+// export { Token } from './token.entity';
+// export { Role } from './role.entity';
+// export { Permission } from './permission.entity';
